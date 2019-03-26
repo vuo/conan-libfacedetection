0bd4dc0792ae1365af48966e9980588c8e9e330e
Steve Mokris <smokris@softpixel.com>
2019-03-26 16:52:39 -0400

    Build both AVX2 (since it's 4x faster) and non-AVX2 (for pre-2015 Mac systems) versions of the library. [#9945]

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class LibfacedetectionConan(ConanFile):
 
     # libfacedetection has no tagged releases, so just use package_version.
     source_version = '0'
-    package_version = '1'
+    package_version = '2'
     version = '%s-%s' % (source_version, package_version)
 
     build_requires = 'llvm/3.3-5@vuo/stable'
@@ -26,29 +26,47 @@ class LibfacedetectionConan(ConanFile):
         self.run('mv %s/LICENSE %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
     def build(self):
+        cmake = CMake(self)
+
+        cmake.definitions['CMAKE_BUILD_TYPE'] = 'Release'
+        cmake.definitions['CMAKE_CXX_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang++'
+        cmake.definitions['CMAKE_C_COMPILER']   = self.deps_cpp_info['llvm'].rootpath + '/bin/clang'
+        cmake.definitions['CMAKE_C_FLAGS'] = cmake.definitions['CMAKE_CXX_FLAGS'] = '-Oz -DNDEBUG'
+        cmake.definitions['CMAKE_CXX_FLAGS'] += ' -stdlib=libc++ -I' + ' -I'.join(self.deps_cpp_info['llvm'].include_paths)
+        cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64'
+        cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.10'
+
+        cmake.definitions['CMAKE_INSTALL_NAME_DIR'] = '@rpath'
+        cmake.definitions['CMAKE_BUILD_WITH_INSTALL_NAME_DIR'] = 'ON'
+        # Workaround for Travis CI's cmake version prior to 3.9:
+        if platform.system() == 'Darwin':
+            cmake.definitions['CMAKE_SHARED_LINKER_FLAGS'] = '-Wl,-install_name,@rpath/libfacedetection.dylib'
+
+        cmake.definitions['ENABLE_INT8'] = 'OFF'
+
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
-            cmake = CMake(self)
+            cmake.definitions['ENABLE_AVX2'] = 'OFF'
 
-            cmake.definitions['CMAKE_BUILD_TYPE'] = 'Release'
-            cmake.definitions['CMAKE_CXX_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang++'
-            cmake.definitions['CMAKE_C_COMPILER']   = self.deps_cpp_info['llvm'].rootpath + '/bin/clang'
-            cmake.definitions['CMAKE_C_FLAGS'] = cmake.definitions['CMAKE_CXX_FLAGS'] = '-Oz -DNDEBUG'
-            cmake.definitions['CMAKE_CXX_FLAGS'] += ' -stdlib=libc++ -I' + ' -I'.join(self.deps_cpp_info['llvm'].include_paths)
-            cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64'
-            cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.10'
+            cmake.configure(source_dir='../%s' % self.source_dir,
+                            build_dir='.')
+            cmake.build()
 
-            cmake.definitions['CMAKE_INSTALL_NAME_DIR'] = '@rpath'
-            cmake.definitions['CMAKE_BUILD_WITH_INSTALL_NAME_DIR'] = 'ON'
-            # Workaround for Travis CI's cmake version prior to 3.9:
-            if platform.system() == 'Darwin':
-                cmake.definitions['CMAKE_SHARED_LINKER_FLAGS'] = '-Wl,-install_name,@rpath/libfacedetection.dylib'
-
+        tools.mkdir(self.build_dir + '_avx2')
+        with tools.chdir(self.build_dir + '_avx2'):
             cmake.definitions['ENABLE_AVX2'] = 'ON'
 
             cmake.configure(source_dir='../%s' % self.source_dir,
                             build_dir='.')
             cmake.build()
+
+            if platform.system() == 'Darwin':
+                libext = 'dylib'
+            elif platform.system() == 'Linux':
+                libext = 'so'
+            else:
+                raise Exception('Unknown platform "%s"' % platform.system())
+            self.run('mv libfacedetection.%s libfacedetection_avx2.%s' % (libext, libext))
 
     def package(self):
         if platform.system() == 'Darwin':
@@ -60,9 +78,10 @@ class LibfacedetectionConan(ConanFile):
 
         self.copy('*.h', src='%s/src' % self.source_dir, dst='include')
         self.copy('libfacedetection.%s' % libext, src='%s' % self.build_dir, dst='lib')
+        self.copy('libfacedetection_avx2.%s' % libext, src='%s_avx2' % self.build_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
     def package_info(self):
-        self.cpp_info.libs = ['facedetection']
+        # Don't set self.cpp_info.libs, since the client needs to decide which of the 2 libraries to use.
         self.cpp_info.includedirs = ['include']
